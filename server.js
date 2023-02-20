f1b7be9438abefaf96826fe724b2a9119baca8c0
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-20 14:54:38 +0000

    find all request added - need to check

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const port = 3000;
 
 //TODO: Create your GET Request Route Below: dd
 
+app.get('/Restaurant', async (request, response) => {
+    //const date = new Date();
+    let allRestuarants = await Restaurant.findAll();
+    response.send(allRestaurants);
+})
+
+
+
 
 app.listen(port, () => {
     sequelize.sync();
